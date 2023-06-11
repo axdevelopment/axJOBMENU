c5ac240a488800ef186ca9aa7b877556eb0a3b56
Ardo <90957355+axdevelopment@users.noreply.github.com>
2023-06-11 21:15:42 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'ardo'
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
+
+server_scripts { '@oxmysql/lib/MySQL.lua', 'server.lua' }
+
+ui_page 'index.html'
+
+files { '*.*'}
